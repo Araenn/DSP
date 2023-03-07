1f00ddfbc7f79d7ce6bb674a0c8ef33f9d86f1d7
Léa Yeromonahos <51425332+Araenn@users.noreply.github.com>
2023-03-07 15:02:47 +0100

    Add files via upload

--- TP4/TP4_1.m
+++ TP4/TP4_1.m
@@ -8,32 +8,40 @@ myWriter = dsp.AudioFileWriter("myOutput.wav", "SampleRate", Fs);
 myScope = timescope("SampleRate", Fs, "YLimits", [-1, 1]);
 %mySpec = dsp.SpectrumAnalyzer("SampleRate", Fs, "PlotAsTwoSidedSpectrum", false);
 
-fp = 4000; %frequence porteuse
+fp = 4000/Fs; %frequence porteuse
 a = 0.05; %constante boucle
 
 fc = 0.2; %frequence coupure
 ordre = 20;
 h = fir1(ordre, 2*fc/Fs, "low"); %fpb phase lineaire avec frquence coupure numerique
 
-state = [];
 p_old = 0;
 cos_sortie = 0;
+sin_sortie = 0;
+comparateur_phase = zeros(N, 1);
+audio_out = zeros(N, 1);
 while ~isDone(myReader)
     audio_in = myReader();
     signal_module = audio_in;
-    comparateur_phase = signal_module .* cos_sortie; 
-    [comparateur_phase, state] = filter(h, 1, comparateur_phase, state);
-
-    phi = angle(comparateur_phase); % phase du signal apres comparateur de phase et fpb
-    delta = 1/2 * phi; % correction de phase
-    d = a * delta; % phase instantanee
-    cos_sortie = cos(pi*(p_old + 2*fp + d)); % en sortie d'increment de phase
-    p = p_old + 2*fp; %increment de phase
-
-    audio_out =  cos_sortie .* signal_module;
     
-    myWriter(audio_out);
+    for n = 1:length(signal_module)        
+        %% PLL
+        signal_in = signal_module(n) .* cos_sortie; 
+        delta = sum(h.*signal_in); %filtrage passe-bas
+        %delta = 0;
+        d = a * delta; % phase instantanee
+        p = p_old + 2*fp + d; %increment de phase
+        cos_sortie = cos(pi*p); % en sortie d'increment de phase
+        sin_sortie = sin(pi*p);
+        
+        p_old = p;
+        audio_out(n) = sin_sortie;
+        
+    end
+    
     myScope([audio_in audio_out]);
+    myWriter(audio_out);
+    
     
 end
 
